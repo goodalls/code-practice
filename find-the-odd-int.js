c96607293963719189e78f1d8ff2878260ed0144
Jeff Goodall <goodallhj@gmail.com>
2018-04-20 21:35:31 -0600

    retypeing function to practice

--- find-the-odd-int.js
+++ find-the-odd-int.js
@@ -1,22 +1,32 @@
 function findOdd(A) {
-  const reduce = A.reduce((acc, num)=>{
-  if (!acc[num]){
-    acc[num] = 0
-  }
-  acc[num]++
-  return acc
-  }, {})
-  return Number(Object.keys(reduce).find(key=>reduce[key] % 2 !== 0))
-  
+  const reduce = A.reduce((acc, num) => {
+    if (!acc[num]) {
+      acc[num] = 0;
+    }
+    acc[num]++;
+    return acc;
+  }, {});
+  return Number(Object.keys(reduce).find(key => reduce[key] % 2 !== 0));
 }
 
-// function findOdd(A) {
-//   var obj = {};
-//   A.forEach(function(el){
-//     obj[el] ? obj[el]++ : obj[el] = 1;
-//   });
-  
-//   for(prop in obj) {
-//     if(obj[prop] % 2 !== 0) return Number(prop);
-//   }
-// }
+function findOdd(A) {
+  var obj = {};
+  A.forEach(function (el) {
+    obj[el] ? obj[el]++ : (obj[el] = 1);
+  });
+
+  for (prop in obj) {
+    if (obj[prop] % 2 !== 0) return Number(prop);
+  }
+}
+
+const findOdd = A => {
+  const obj = {};
+  A.forEach(num => (obj[num] ? obj[num]++ : (obj[num] = 1)));
+
+  for (num in obj) {
+    if (obj[num] % 2 !== 0) {
+      return Number(num);
+    }
+  }
+};
