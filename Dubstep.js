afdd1800bf056eaa536c6de1f35442944b32a1a0
Jeff Goodall <goodallhj@gmail.com>
2018-04-27 08:43:51 -0600

    still not working

--- Dubstep.js
+++ Dubstep.js
@@ -1,17 +1,16 @@
-function songDecoder(song){
-  const array = song.split('');
-  const filter = array.forEach((letter, index)=>{
-  if (letter === 'W' || index+1 === 'U' || index+2 === 'B') {
-    array.splice(index, 3, ' ');
-  } else {
-  return letter
-  }
-  })
-  const solution = array.filter(letter=>letter.trim())
-  return solution.join('').trim()
+function songDecoder(song) {
+  const solution = [...song];
+ 
+ for (let i = 0; i<solution.length; i++) {
+   if(solution[i] === 'W' && solution[i+1] === 'U' && solution[i+2] === 'B') {
+     console.log('wub trigered');
+     solution.splice(i, 3)
+   } 
+ }
+ return solution
 }
 
 
-console.log(songDecoder("AWUBBWUBC"))// "A B C","WUB should be replaced by 1 space");
-console.log(songDecoder("AWUBWUBWUBBWUBWUBWUBC"))// "A B C","multiples WUB should be replaced by only 1 space");
-console.log(songDecoder("WUBAWUBBWUBCWUB"))// "A B C","heading or trailing spaces should be removed");
+console.log(songDecoder("AWUBBWUBC")) // "A B C","WUB should be replaced by 1 space");
+console.log(songDecoder("AWUBWUBWUBBWUBWUBWUBC")) // "A B C","multiples WUB should be replaced by only 1 space");
+console.log(songDecoder("WUBAWUBBWUBCWUB")) // "A B C","heading or trailing spaces should be removed");
