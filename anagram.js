4355bd0e7da87395ff7fcb4d716c35308a3579c5
Jeff Goodall <goodalls@live.com>
2018-05-09 15:51:02 -0600

    Updating Anagram.js

--- anagram.js
+++ anagram.js
@@ -6,7 +6,5 @@ function anagrams(word, words) {
 }
 
 console.log(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']))// => ['aabb', 'bbaa']
-
 console.log(anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']))// => ['carer', 'racer']
-
 console.log(anagrams('laser', ['lazing', 'lazy', 'lacer']))// => []
