02e414b01637f10f738d9be347b85c860533a945
Jeff Goodall <goodalls@live.com>
2018-05-09 15:48:59 -0600

    adding anagram.js

---
+++ anagram.js
@@ -0,0 +1,12 @@
+function anagrams(word, words) {
+  word = word.split('').sort().join('');
+  return words.filter(oneWord => {
+    return word == oneWord.split('').sort().join('');
+  });
+}
+
+console.log(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']))// => ['aabb', 'bbaa']
+
+console.log(anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']))// => ['carer', 'racer']
+
+console.log(anagrams('laser', ['lazing', 'lazy', 'lacer']))// => []
