c1f3753867cf309692c7be3eaeeb18c8aaf38911
Jeff Goodall <goodalls@live.com>
2018-05-22 15:16:03 -0600

    refactor snail for understanding

--- snail.js
+++ snail.js
@@ -35,18 +35,18 @@ const snail = (array) => {
   while (array.length) {
 
     // Steal the first row
-    result = result.concat(array.shift());
+    result = [...result, ...array.shift()];
 
     // Steal the right items
-    for (var i = 0; i < array.length; i++) {
+    for (let i = 0; i < array.length; i++) {
       result.push(array[i].pop());
     }
 
     // Steal the bottom row
-    result = result.concat((array.pop() || []).reverse());
-
+    // result = result.concat((array.pop() || []).reverse());
+    result = [...result, ...array.pop()||[].reverse()]
     // Steal the left items
-    for (var i = array.length - 1; i >= 0; i--) {
+    for (let i = array.length - 1; i >= 0; i--) {
       result.push(array[i].shift());
     }
   }
