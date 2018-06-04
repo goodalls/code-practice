42613472392908d3f33bd5453623f2d7dea11b75
Jeff Goodall <goodalls@live.com>
2018-06-04 13:18:46 -0600

    adding bitcounting

---
+++ bitCounting.js
@@ -0,0 +1,15 @@
+/*
+Write a function that takes an (unsigned) integer as input, and returns the number of bits that are equal to one in the binary representation of that number.
+
+Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+*/
+
+var countBits = function(n) {
+  return n.toString(2).split('').reduce((a,b)=>a+Number(b),0)
+};
+
+console.log(countBits(0)) //, 0);
+console.log(countBits(4)) //, 1);
+console.log(countBits(7)) //, 3);
+console.log(countBits(9)) //, 2);
+console.log(countBits(10)) //, 2);
