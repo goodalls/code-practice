8f65c754e2d8e4622e5640acfae22bc30216e8dd
Jeff Goodall <goodalls@live.com>
2019-06-23 20:59:39 -0600

    adding findSmallestIntInArray.js

---
+++ findSmallestIntInArray.js
@@ -0,0 +1,10 @@
+
+const findSmallestInt = (args) => {
+    return Math.min(...args)
+  }
+
+    console.log(findSmallestInt([78,56,232,12,8]))//8
+    console.log(findSmallestInt([78,56,232,12,18]))//12
+    console.log(findSmallestInt([78,56,232,412,228]))//56
+    console.log(findSmallestInt([78,56,232,12,0]))//0
+    console.log(findSmallestInt([1,56,232,12,8]))//1
